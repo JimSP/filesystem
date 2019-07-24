6563c103581258a8cb0d802d4acc991893b7e061
alexandre <alexandre.msl@gmail.com>
2019-07-24 00:38:42 -0300

    refavtoring ftpFileCreator

---
+++ src/main/java/br/com/cafebinario/filesystem/ftp/FtpFileCreator.java
@@ -0,0 +1,230 @@
+package br.com.cafebinario.filesystem.ftp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.ftpserver.ftplet.FtpFile;
+
+import br.com.cafebinario.filesystem.mappers.PathToFtpFileMapper;
+
+public final class FtpFileCreator {
+
+	public static FtpFile create(final Path source, final FileSystem fileSystem, final PathToFtpFileMapper pathToFtpFileMapper) {
+		
+		return new FtpFile() {
+
+            private final AtomicLong time = new AtomicLong(System.currentTimeMillis());
+            
+            private Path getPath() {
+                
+                return source;
+            }
+
+            @Override
+            public boolean setLastModified(final long time) {
+                
+                if (isBeforeTime(time)) {
+                    this.time.set(time);
+                    return true;
+                }
+
+                return false;
+            }
+            
+            @Override
+            public boolean move(final FtpFile destination) {
+                
+                final Path from = getPath();
+                
+                final Path destinationPath = fileSystem.getPath(destination.getAbsolutePath());
+                
+                try {
+                    
+                    return Files.move(from, destinationPath) != null;
+                    
+                } catch (IOException e) {
+                    
+                    return false;
+                }
+            }
+
+            @Override
+            public boolean mkdir() {
+
+                try {
+                    
+                    return Files.createDirectories(getPath()) != null;
+                    
+                } catch (IOException e) {
+                    
+                    return false;
+                }
+            }
+
+            @Override
+            public List<? extends FtpFile> listFiles() {
+
+                try (final Stream<Path> stream = Files.list(getPath())) {
+
+                    return stream
+                            .map(pathToFtpFileMapper::convert)
+                            .collect(Collectors.toList());
+                    
+                } catch (IOException e) {
+                    
+                    return Collections.emptyList();
+                }
+            }
+
+            @Override
+            public boolean isWritable() {
+
+                return true;
+            }
+
+            @Override
+            public boolean isRemovable() {
+
+                return true;
+            }
+
+            @Override
+            public boolean isReadable() {
+
+                return Files.isReadable(getPath());
+            }
+
+            @Override
+            public boolean isHidden() {
+
+                try {
+                    return Files.isHidden(getPath());
+                } catch (IOException e) {
+                   return false;
+                }
+            }
+
+            @Override
+            public boolean isFile() {
+
+                return !Files.isDirectory(getPath());
+            }
+
+            @Override
+            public boolean isDirectory() {
+
+                return Files.isDirectory(getPath());
+            }
+
+            @Override
+            public long getSize() {
+
+                try {
+                    
+                    return Files.size(getPath());
+                } catch (IOException e) {
+                    
+                   return -1L;
+                }
+            }
+
+            @Override
+            public Object getPhysicalFile() {
+                final long size = getSize();
+                
+                final byte[] buffer = new byte[(int) size];
+                
+                try(final InputStream inputStream = Files.newInputStream(getPath())) {
+                    inputStream.read(buffer);
+                    return buffer;
+                } catch (IOException e) {
+                   return new byte[0];
+                }
+            }
+
+            @Override
+            public String getOwnerName() {
+
+                try {
+                    return Files.getOwner(getPath()).getName();
+                } catch (IOException e) {
+                    return "";
+                }
+            }
+
+            @Override
+            public String getName() {
+
+                return getPath().getFileName().toString();
+            }
+
+            @Override
+            public int getLinkCount() {
+
+                return getPath().getNameCount();
+            }
+
+            @Override
+            public long getLastModified() {
+                return time.get();
+            }
+
+            @Override
+            public String getGroupName() {
+
+                return "";
+            }
+
+            @Override
+            public String getAbsolutePath() {
+
+                return getPath().toAbsolutePath().toString();
+            }
+
+            @Override
+            public boolean doesExist() {
+
+                return Files.exists(getPath());
+            }
+
+            @Override
+            public boolean delete() {
+
+                try {
+                    return Files.deleteIfExists(getPath());
+                } catch (IOException e) {
+                    return false;
+                }
+            }
+
+            @Override
+            public OutputStream createOutputStream(final long offset) throws IOException {
+
+                return Files.newOutputStream(getPath());
+            }
+
+            @Override
+            public InputStream createInputStream(final long offset) throws IOException {
+
+                return Files.newInputStream(getPath());
+            }
+            
+            private boolean isBeforeTime(final long time) {
+				return this.time.get() < time;
+			}
+        };
+	}
+	
+	private FtpFileCreator() {
+		
+	}
+}
